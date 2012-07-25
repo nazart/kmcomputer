d3d34361994b6b28a3ed5a3bfc9de37f223f4918
nazart jara huaman <nazarjb@hotmail.com>
2012-07-24 22:17:51 -0500

    cambios nazart

--- public/static/js/kmcomputer.utils.js
+++ public/static/js/kmcomputer.utils.js
@@ -1,30 +1,106 @@
 $(document).ready(function(){
+    
+    /*despligue del menu*/
+    /*----------------------------------*/
+    $('.navigation > li').hover(
+        function () {
+            //show its submenu
+            $('ul:first', this).slideDown(100);
+        }, 
+        function () {
+            //hide its submenu
+            $('ul:first', this).slideUp(100);			
+        }
+        );
+    $('.navigation > li > ul > li').hover(
+        function () {
+            //show its submenu
+            $('ul:first', this).slideDown(100);
+        }, 
+        function () {
+            //hide its submenu
+            $('ul:first', this).slideUp(100);			
+        }
+        );
+    /*----------------------------------*/
+
+
+
     /*agregar productos al carrito de compras*/
+    /*----------------------------------*/
+
     $('a[id^="ajax_id_product"]').click(function(event){
         event.preventDefault();
         cadena=this.id;
         cadena = cadena.replace('ajax_id_product','form_pres');
         $('#'+cadena).submit();
     });
+    /*----------------------------------*/
+
+
     
     /*eliminar productos del carrito de compras*/
     $('a[id^="eliminarCarrito_"]').click(function(event){
         event.preventDefault();
         cadena=this.id;
         cadena = cadena.replace('eliminarCarrito_','ItemCarrito_');
+        if(confirmDialog('asdads')==true){
+            alert('menaaje');
+        }
+        var param = new Array(2);
+        param[0] = new Array(2);
+        param[0][0] = this;
+        param[0][1] = cadena;
+        confirmDialog('mensaje',eliminarCarritoAjax,'',param)
+    });
+    
+    function eliminarCarritoAjax(obj,divContent){
+ 
+  
         $.ajax({
-            url: ($('#'+this.id).attr("href")),
+            url: ($('#'+obj.id).attr("href")),
             success: function(data) {
                 $('.result').html(data);
             }
         });
+        if(divContent!=''){
+            $('#'+divContent).hide("slow");
+        }
         
-        $('#'+cadena).hide("slow");
         
-        //alert($('#ListProduct').length);
-        
-    });
+    }
     
+    function confirmDialog(mensaje,functionTrue, functionFalse, param){
+        var callbacks = $.Callbacks();
+        $("#dialog-confirm").dialog({
+            resizable: false,
+            height:140,
+            modal: true,
+            buttons: {
+                "Eliminar": function() {
+                    if(functionTrue!=''){
+                        callbacks.add(functionTrue);
+                        paramsTrue = param[0];
+                        console.debug(param[0]);
+                        callbacks.fireWith(window,paramsTrue);
+                    }
+                    $( this ).dialog( "close" );
+                    return true;            
+                },
+                "Calcelar": function() {
+                    if(functionFalse!=''){
+                        callbacks.add(functionFalse);
+                        paramsFalse = param[1];
+                        callbacks.fireWith(window,paramsFalse);
+                    }
+                    $( this ).dialog( "close" );
+                    return false;
+                }
+            }
+        });
+        
+    }
     
-   
+//$("#confirmDialog").dialog()
+    
 });
