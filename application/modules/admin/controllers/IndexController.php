28167a2bcfdd4dd775447d1a5ae136d38842b581
nazart jara huaman <nazarjb@hotmail.com>
2012-07-28 20:34:05 -0500

    cambios nazart

---
+++ application/modules/admin/controllers/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of BuscarController
+ *
+ * @author Laptop
+ */
+class Admin_IndexController extends CST_Controller_ActionAdmin {
+
+    public function init() {
+        parent::init();
+        /* Initialize action controller here */
+    }
+
+    public function indexAction() {
+                //$modelBusqueda = new Application_Model_Busqueda();
+    }
+    //put your code here
+}
+
+?>
